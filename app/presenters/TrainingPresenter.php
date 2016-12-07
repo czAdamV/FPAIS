c4875f64ee16d15a6790d81082c588a95370e122
novotvi4 <novotvi4@b15cdab5-9911-459d-b0db-d5261b21a395>
2016-12-07 14:04:44 +0000

    addad action list
    git-svn-id: https://svn.project.fit.cvut.cz/FPAIS/trunk/src@71 b15cdab5-9911-459d-b0db-d5261b21a395

--- app/presenters/TrainingPresenter.php
+++ app/presenters/TrainingPresenter.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace FPAIS\Presenters;
+
 /**
  * Description of TrainingPresenter
  *
  * @author viky
  */
-class TrainingPresenter extends \Nette\Application\UI\Presenter{
-    //put your code here
+class TrainingPresenter extends \Nette\Application\UI\Presenter {
+
+    public function actionList() {
+        $this->template->trainings = [['place'=>'none']];
+    }
+
 }
