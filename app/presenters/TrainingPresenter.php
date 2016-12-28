39f79302a2dfcf01a17be9a6f3f5a0d963e064c2
novotvi4 <novotvi4@b15cdab5-9911-459d-b0db-d5261b21a395>
2016-12-28 15:18:52 +0000

    pěkně dloho nefungoval svn server, takže je to hromada všeho možného i nemožného (hlavně si to už nepamatuju) god bless git
    git-svn-id: https://svn.project.fit.cvut.cz/FPAIS/trunk/src@117 b15cdab5-9911-459d-b0db-d5261b21a395

--- app/presenters/TrainingPresenter.php
+++ app/presenters/TrainingPresenter.php
@@ -21,6 +21,10 @@ class TrainingPresenter extends \Nette\Application\UI\Presenter {
 
     public function createComponentNewTrainingForm(): \Nette\Application\UI\Form {
         $form = new \Nette\Application\UI\Form();
+        $form->addDateTimePicker('date', 'Date');
+        $form->addSelect('place', 'Místo: ', ['placeholder' => 'place holder - missing manger to get list of places']);
+        $form->addInteger('min', 'Minimum hráčů');
+        $form->addInteger('max', 'Maximum hráčů');
         $form->addSubmit('create', 'vytvořit');
         $form->onSuccess[] = [$this, 'onSubmitNewTraining'];
         return $form;
