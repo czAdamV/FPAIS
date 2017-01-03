bfc0ca79ced8c40cec1cc03b24181bab3376901b
novotvi4 <novotvi4@b15cdab5-9911-459d-b0db-d5261b21a395>
2017-01-03 16:46:39 +0000

    je třeba volat parrent::startup
    git-svn-id: https://svn.project.fit.cvut.cz/FPAIS/trunk/src@128 b15cdab5-9911-459d-b0db-d5261b21a395

--- app/presenters/templates/BasePresenter.php
+++ app/presenters/templates/BasePresenter.php
@@ -5,6 +5,7 @@ namespace FPAIS\Presenters;
 class BasePresenter extends \Nette\Application\UI\Presenter {
 
     public function startup() {
+        parent::startup();
         if (!$this->getUser()->isLoggedIn()) {
             $this->redirect('Sign:in');
         }
